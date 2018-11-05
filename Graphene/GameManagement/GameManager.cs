90f856426e72cd9eae01d679fbdf4d55df3d6cc4
Yuri Cruz <yuriaicruz@gmail.com>
2018-11-04 23:52:50 -0200

    import

---
+++ Graphene/GameManagement/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameManagement
+{
+    public abstract class GameManager : MonoBehaviour
+    {
+        public event Action LevelStart, PlayerDie, Pause, UnPause, GameOver;
+
+        private bool _paused;
+
+        public virtual void OnGameOver()
+        {
+            if (GameOver != null) GameOver();
+        }
+
+        public virtual void OnPlayerDie()
+        {
+            if (PlayerDie != null) PlayerDie();
+        }
+
+        public virtual void OnLevelStart()
+        {
+            if (LevelStart != null) LevelStart();
+        }
+
+        public void OnPause()
+        {
+            _paused = !_paused;
+
+            if (_paused)
+            {
+                if (Pause != null) Pause();
+            }
+            else if (UnPause != null) UnPause();
+        }
+    }
+}
---
+++ Graphene/GameManagement/SaveData.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace GameManagement
+{
+    public static class SaveData
+    {
+        public static string SavePath = "save.dat";
+
+        private static string _dataPath;
+
+        private static void CheckDataPath()
+        {
+            if (string.IsNullOrEmpty(_dataPath))
+            {
+#if UNITY_EDITOR
+                _dataPath = Application.dataPath + "/../saves/";
+#else
+                _dataPath = Application.persistentDataPath + "/saves/";
+#endif
+            }
+        }
+
+        public static void Save(object data, string name)
+        {
+            CheckDataPath();
+
+            var path = _dataPath + name + ".dat";
+
+            Directory.CreateDirectory(Path.GetDirectoryName(path));    
+
+            File.WriteAllText(path, JsonConvert.SerializeObject(data));
+        }
+
+        public static T Load<T>(string name)
+        {
+            CheckDataPath();
+
+            var path = _dataPath + name + ".dat";
+
+            if (!File.Exists(path)) return default(T);
+
+            var data = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+}
